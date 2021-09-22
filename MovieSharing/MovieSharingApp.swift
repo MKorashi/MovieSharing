3b7f5311e21a01c35642d420710150f032556ee7
Mariam Korashi <mariam.korashi@gmail.com>
2021-09-22 13:15:05 +0200

    First Commit

---
+++ MovieSharing/MovieSharingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSharingApp.swift
+//  MovieSharing
+//
+//  Created by Mariam Korashi on 22.09.21.
+//
+
+import SwiftUI
+
+@main
+struct MovieSharingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
